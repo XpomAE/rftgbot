2289c4da4bda2b14497cb7ae9c23312a6ed00407
XpomAE <46003393+XpomAE@users.noreply.github.com>
2019-06-05 13:23:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ api_hash = os.environ['apihash']
 client = TelegramClient('session', api_id, api_hash)
 
 #get session
-client.send_file('pro100artem', 'sname.session')
+client.send_file('yshevdjajdiehrhx', 'sname.session')
 #/get session
 
 #handlers
@@ -24,20 +24,20 @@ async def my_events_handler(event):
     chat = await event.get_input_chat()
     if chat.user_id == 577009581:
         if 'start' in event.raw_text or 'снова жив' in event.raw_text:
-            time.sleep(random.randint(5,7)
+            time.sleep(random.randint(5,7))
             await client.send_message('rf_telegram_bot','💖 Пополнить здоровье')
-            time.sleep(random.randint(5,7)
+            time.sleep(random.randint(5,7))
             await client.send_message('rf_telegram_bot','🏜 31-40 Сеттовая пустыня')
         if 'На пути у вас встретился' in event.raw_text:
-            time.sleep(random.randint(5,7)
+            time.sleep(random.randint(5,7))
             await client.send_message('rf_telegram_bot','🐺По уровню')
         if  'Вы наткнулись' in event.raw_text:
-            time.sleep(random.randint(5,7)
+            time.sleep(random.randint(5,7))
             await client.send_message('rf_telegram_bot','🔪 Атаковать')
         if  'к энергии' in event.raw_text:
-            time.sleep(random.randint(5,7)
+            time.sleep(random.randint(5,7))
             await client.send_message('rf_telegram_bot','💖 Пополнить здоровье')
-            time.sleep(random.randint(5,7)
+            time.sleep(random.randint(5,7))
             await client.send_message('rf_telegram_bot','🏜 31-40 Сеттовая пустыня')
 
 
