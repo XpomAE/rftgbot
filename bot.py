d90cd53a98bdc61b0afef446e00b1422a42e436a
XpomAE <46003393+XpomAE@users.noreply.github.com>
2019-06-08 10:15:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,7 @@ from emoji import emojize
 
 from telethon import TelegramClient, events, sync
 api_id = 541670['apiid']
-api_hash = 
-e4b71141f5b96dfd5585659170d8aafa ['apihash']
+api_hash = e4b71141f5b96dfd5585659170d8aafa ['apihash']
 client = TelegramClient('session', api_id, api_hash)
 
 #get session
